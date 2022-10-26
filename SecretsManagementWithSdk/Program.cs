09d862286ea85e83b6a530265e6bcf03e304c00e
Yongguang Zhu <yoz@microsoft.com>
2022-10-26 23:38:50 +0800

    add SecretsManagementWithSdk project; upgrade nuget

---
+++ SecretsManagementWithSdk/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapr.Client;
+
+Console.WriteLine("Hello, World!");
+
+string SECRET_STORE_NAME = "localsecretstore";
+//string SECRET_STORE_NAME = "azurekeyvault";
+using var client = new DaprClientBuilder().Build();
+//Using Dapr SDK to get a secret
+var secret = await client.GetSecretAsync(SECRET_STORE_NAME, "secret");
+//var secret = await client.GetSecretAsync(SECRET_STORE_NAME, "cae-connstr");
+Console.WriteLine($"Result: {string.Join(", ", secret)}");
---
+++ SecretsManagementWithSdk/README.md
@@ -0,0 +1,7 @@
+﻿```sh
+dapr run --app-id quickstarts-scmsdk --dapr-http-port 13504 --components-path ../components -- dotnet run
+```
+
+Demostrates secrets management sdk usage:
+
+- get secret
